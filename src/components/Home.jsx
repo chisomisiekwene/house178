acc7b31860dce85ff332156ef2e51f8fa9814261
chisomisiekwene <chisomisiekwene57@gmail.com>
2023-02-02 13:27:17 +0100

    navbar styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,34 @@
 import React from "react";
 import '../styles/Home.css'
+import { useState } from "react";
 
 function Home(){
+    const [isActive, setIsActive] = useState(false);
+
+    function toggleActive(){
+        setIsActive(prevState => !prevState)
+    }
+    
     return(
         <div className="homepage" style={{backgroundImage:`url("images/background.png")`}} id="Home">
             <div className="house178">
                 <div className="logoContainer">
                     <img className="logo" src="images/logo.png" alt="house178 logo"></img>
-                    <h3>HOUSE 178</h3>
                 </div>
-               <div className="nav">
+
+                <div className="menubar" onClick={toggleActive}>
+                    <img src="images/menu-icon.png" alt="menu icon" className="menu"/>
+                </div>
+
+               <div className={`nav ${isActive ? 'active' : ''}`}>
                 <ul className="navbar">
-                    <li className="navlink"><a href="#Home">HOME</a></li>
-                    <li className="navlink"><a href="#About">ABOUT</a></li>
-                    <li className="navlink"><a href="#contact">CONTACT</a></li>
-                    <li className="navlink"><a href="#services">SERVICES</a></li>
+                    <li className="navlink"><a href="#Home" onClick={() => setIsActive(false)}>HOME</a></li>
+                    <li className="navlink"><a href="#About" onClick={() => setIsActive(false)}>ABOUT</a></li>
+                    <li className="navlink"><a href="#contact" onClick={() => setIsActive(false)}>CONTACT</a></li>
+                    <li className="navlink"><a href="#services" onClick={() => setIsActive(false)}>SERVICES</a></li>
+                    <li className="navlink"><a href="#gallery" onClick={() => setIsActive(false)}>GALLERY</a></li>
                 </ul>
-                <img src="images/menu-icon.png" alt="menu icon" className="menu"/>
+                
                </div>
             </div>
 
@@ -29,7 +41,7 @@ function Home(){
                     <a href="www.google.com">Book a space</a>
                 </div>
                 <div className="location">
-                    <img src="images/icon.png" alt="location icon"></img>
+                    <img className="location-icon" src="images/icon.png" alt="location icon"></img>
                     <p>House 178 Lounge, Airways Oyemekun Road. Akure.</p>
                 </div>
             </div>
